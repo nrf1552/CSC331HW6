9e6dd699ddea7603dff2ef7f9f455cf537d03435
Daniel Emery <Daniel Emery@172.21.136.96>
2016-10-14 12:16:44 -0400

    troubleshooting MathEngine

--- CSC331HW6/src/HW6/MathEngine.java
+++ CSC331HW6/src/HW6/MathEngine.java
@@ -1,10 +1,78 @@
 package HW6;
 
-public class MathEngine {
+import java.util.Random;
+import java.util.Scanner;
 
+public class MathEngine {
+	
+	private int fixedNum;
+	private int randomNum;
+	
 	public MathEngine() {
 		// TODO Auto-generated constructor stub
 	}
-
+	
+	public static void main(String[] args) {
+		
+		//Creates a fixed variable based on the user's input
+		System.out.println("What number would you like your questions to be based on? (0 - 12): ");
+		Scanner range = new Scanner(System.in);
+		int fixedNum = range.nextInt();
+		System.out.println(fixedNum);
+		
+		//Create random number between 1 and 12
+		int maxRange = 12;
+		int randomNum = (int) (Math.random() * maxRange + 1);
+		System.out.println(randomNum);
+		
+		//Let user decide if they want to add or subtract
+		System.out.println("Add/Subtract (1) or Multiply/Divide (2)?: ");
+		Scanner user = new Scanner(System.in);
+		int userNum = user.nextInt();
+		if (userNum == 1) {
+			//addSubtract();
+		}
+		
+		else if (userNum == 2) {
+			//multDivide();
+		}
+		
+	}
+	
+	public int addSubtract() {
+		
+		//Random addsubInteger = new Random();
+		int addsubInteger = (int) (Math.random() * 2 + 1);
+		System.out.println(addsubInteger);
+		
+		if (addsubInteger == 1) {
+			System.out.println(fixedNum + " + " + randomNum);
+			return fixedNum + randomNum;
+		}
+		
+		else {
+			System.out.println(fixedNum + " - " + randomNum);
+			return fixedNum - randomNum;
+		}
+		
+	} 
+	
+	public int multDivide() {
+		
+		//Random multdivInteger = new Random;
+		int multdivInteger = (int) (Math.random() * 2 + 1);
+		System.out.println(multdivInteger);
+		
+		if (multdivInteger == 1) {
+			System.out.println(fixedNum + " * " + randomNum);
+			return fixedNum * randomNum;
+		}
+		
+		else {
+			System.out.println(fixedNum + " / " + randomNum);
+			return fixedNum / randomNum;
+		}
+		
+	}
 	
 }
