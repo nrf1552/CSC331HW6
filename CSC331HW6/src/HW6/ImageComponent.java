fee61a48e9fbfd03459a29155761d0fa82649e4e
Nick <Nick@172.21.127.225>
2016-10-19 13:09:12 -0400

    Update from today

--- CSC331HW6/src/HW6/ImageComponent.java
+++ CSC331HW6/src/HW6/ImageComponent.java
@@ -6,23 +6,23 @@ import java.awt.Component;
 import java.awt.Graphics;
 import java.awt.image.*;
 
-public class ImageComponent extends JPanel{
+public class ImageComponent extends JPanel {
 
 	public MathEngine math;
 
 	private JLabel workingLayer;
 	private BufferedImage finalImage;
 	private String problem;
-	
+
 	int width;
 	int height;
 
-	public ImageComponent(BufferedImage image, int userInput, int mathType) {
+	public ImageComponent(BufferedImage image, int userInput, boolean isAddSubtract) {
 		width = image.getWidth();
 		height = image.getHeight();
 		workingLayer = new JLabel();
-		math = new MathEngine();
-		
+		math = new MathEngine(userInput, isAddSubtract);
+
 		finalImage = image;
 		// problem = math.getProblem();
 
@@ -30,37 +30,26 @@ public class ImageComponent extends JPanel{
 	}
 
 	private void showTopLayer() {
-		Icon icon = new Icon() {			
-			@Override
-			public void paintIcon(Component c, Graphics g, int x, int y) {}
-			
-			@Override
-			public int getIconWidth() {return width;}
-			
-			@Override
-			public int getIconHeight() {return height;}
-		};
-		
-		
+		new TopPanel(height, width).show();
 	}
 
 	private void showMathLayer() {
-		
-		// display problem
+		String problem = math.getProblem();
 
-		// get user input
+		// show problem
+		MathPanel panel = new MathPanel(problem, 0, 0);
 
-		// if (math.isCorrect(6)){
-		// showImageLayer();
-		// }
-
+		// check answer
+		if (math.isCorrect(6)) {
+			showImageLayer();
+		}
 	}
 
 	private void showImageLayer() {
 
 	}
-	
-	public static void main(){
-		
+
+	public static void main() {
+
 	}
 }
---
+++ CSC331HW6/src/HW6/TopPanel.java
@@ -0,0 +1,17 @@
+package HW6;
+
+import javax.swing.JPanel;
+
+public class TopPanel {
+	public int panelHeight;
+	public int panelWidth;
+	
+	public TopPanel(int height, int width){
+		
+	}
+		
+	// return a sized grey top panel
+	public JPanel show(){
+		return null;
+	}
+}
