ba1adaedd19a5aedcb7cb5a17057467e1cc9a2be
Nick <Nick@172.21.127.225>
2016-10-19 12:35:22 -0400

    Math panel framework

---
+++ CSC331HW6/src/HW6/MathPanel.java
@@ -0,0 +1,33 @@
+package HW6;
+
+import javax.swing.JPanel;
+
+public class MathPanel {
+	
+	private int panelHeight;
+	private int panelWidth;
+	private String mathProblem;
+	
+	public MathPanel(String problem, int height, int width){
+		mathProblem = problem;
+		panelHeight = height;
+		panelWidth = width;
+	}
+	
+	public JPanel show(){
+		JPanel p = new JPanel();
+		//resize panel
+		
+		//show problem in lab
+		
+		//show textbox for user entry
+	
+		//show submit button
+		
+		//pack and make visible
+		
+		//return panel
+		return p;
+	}
+
+}
